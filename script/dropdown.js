12b957e4a8782be18e79b9469b50cf15e3f5f4bb
IconicPrefix <iconicprefix@gmail.com>
2024-06-24 23:59:58 -0700

    Drop down

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-    $("")
+$(document).ready(function () {
+    $(".icon.burger-menu.dark").on("click", function () {
+        $(".menu").toggleClass("show hidden");
+    });
+
+});
+
+$(document).mouseup(function (e) {
+    var container = $(".menu");
+
+    // If the target of the click isn't the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if (container.hasClass("show")) {
+            container.toggleClass("show hidden");
+
+        }
+    }
+
+
 });
